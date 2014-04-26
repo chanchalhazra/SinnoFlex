e4c7654a346771fb6a6b2c46ff18eeef5e446353
Kumar Hazra <kumar.c@truglobal.com>
2014-04-26 16:58:04 -0700

    Initial commit

---
+++ about/contact.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="SinnoFlex is a leading information technology 
+          consulting firm serving clients throughout Globe. Our technology services 
+          and business solutions consultants are experts in providing simple innovative
+          flexible technology solutions to business problems.">
+    <meta name="author" content="Chanchal Hazra">
+    <meta name="keywords" content="IT services, IT consultants, outsource information technology, 
+          application development, IT consulting firm, business process outsourcing, 
+          computer business consultants, healthcare IT consulting, IT strategy, 
+          business technology services, information technology consulting" />
+
+    <title>SinnoFlex consulting Inc | Experts in simple innovative flexible Technology Solutions
+</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.css" rel="stylesheet">
+
+    <!-- Add custom CSS here -->
+    <link href="../css/sinnoflex-main.css" rel="stylesheet">
+    <link href="../font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    
+  </head>
+
+  <body>
+ 	 <div id="header1" style=" height: 65px;width:auto;margin: 0" >
+          <div style=" float:left ;height: 50px;width: 900px;margin: 0;">
+            <a id="SinnoflexLogo" href="./" >
+           <!img src="assets/logo4.png" alt="SinnoFlex" style="float: left;width: 80px; height: 60px"/></a>
+              <h1 style="color: #c09853; margin-left: 50px;">SinnoFlex</h1>
+              <h5 style="color: #c09853">We innovate simple and flexible solutions for our customers.</h5>
+          </div>
+          <div style=" height: 40px;float: left;margin-top:10px; ">
+              <a id="siteheadercontent_0_rptrHeaderMenu_lnkHeaderMenuItem_0" href="about.html" style="color: #c09853;">About</a>
+            <span id="siteheadercontent_0_rptrHeaderMenu_lblLinkSeparator_1">&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+            <a id="siteheadercontent_0_rptrHeaderMenu_lnkHeaderMenuItem_1" href="career.html" style="color: #c09853;">Careers</a>
+            <span id="siteheadercontent_0_rptrHeaderMenu_lblLinkSeparator_3">&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+            <a id="siteheadercontent_0_rptrHeaderMenu_lnkHeaderMenuItem_3" href="contact.php" style="color: #c09853;">Contact</a>
+                
+          </div>
+	    
+   </div>			
+
+    <nav class="navbar navbar-inverse" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <!-- You'll want to use a responsive image option so this logo looks good on devices - I recommend using something like retina.js (do a quick Google search for it and you'll find it) -->
+          <a id="rotateSlogan" class="navbar-brand" href="../index.html">We focus on Flexibility to Scale</a>
+        </div>
+
+         
+          
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse navbar-ex1-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="../services.html">Services</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Business Solutions<b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="../services/BI.html">Business Intelligence</a></li>
+                <li><a href="../services/CRM.html">Customer Relationship Management</a></li>
+                <li><a href="../services/ERP.html">Enterprise Resource Planning</a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Technology Services<b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="../services/WebApps.html">Web Application Development</a></li>
+                <li><a href="../services/MobileApps.html">Mobile Application Development</a></li>
+                <li><a href="../services/CustomApps.html">Custom Application Development</a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Industry News <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="../latestTrend.html">Latest Trends</a></li>
+                <li><a href="../blog.html">Blog</a></li>
+                <li><a href="../faq.html">FAQ</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container -->
+    </nav>
+     
+<script>
+              var mySlogan = document.getElementById('rotateSlogan');
+              var ctr=0;
+              var txtArray = new Array();
+              txtArray[0]="We are inspired by Simplicity and Speed";
+              txtArray[1]="We challenge to Innovate"; 
+              txtArray[2]="We focus on Flexibility to Scale";
+              var myInterval = setInterval(changeText,10000);
+              
+              
+              function changeText()
+              {
+                  ctr++;
+                  if(ctr>2)
+                      ctr=0;
+                  mySlogan.innerHTML = txtArray[ctr];
+              }
+          </script>
+          
+    <!-- Page Content -->
+
+    <div class="container">
+      
+      <div class="row">
+      
+        <div class="col-lg-12">
+          <h1 class="page-header"><small>We'd Love to Hear From You!</small></h1>
+          <ol class="breadcrumb">
+            <li><a href="../index.html">Home</a></li>
+            <li class="active">Contact</li>
+          </ol>
+        </div>
+        
+        <div class="col-lg-12">
+          <!-- Embedded Google Map using an iframe - to select your location find it on Google maps and paste the link as the iframe src. If you want to use the Google Maps API instead then have at it! -->
+          <iframe width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;ll=37.0625,-95.677068&amp;spn=56.506174,79.013672&amp;t=m&amp;z=4&amp;output=embed"></iframe>
+        </div>
+
+      </div><!-- /.row -->
+      
+      <div class="row">
+
+        <div class="col-sm-8">
+          <h3>Let's Get In Touch!</h3>
+          <p>	
+              If you need cloud technology solutions; need a business application; or even if you 
+              have a unique technology problem, we're here to help.
+          </p>
+              <?php  
+
+                // check for a successful form post  
+                if (isset($_GET['s'])) echo "<div class=\"alert alert-success\">".$_GET['s']."</div>";  
+          
+                // check for a form error  
+                elseif (isset($_GET['e'])) echo "<div class=\"alert alert-danger\">".$_GET['e']."</div>";  
+
+			?>
+            <form role="form" method="POST" action="contact-form-submission.php">
+	            <div class="row">
+	              <div class="form-group col-lg-4">
+	                <label for="input1">Name</label>
+	                <input type="text" name="contact_name" class="form-control" id="input1">
+	              </div>
+	              <div class="form-group col-lg-4">
+	                <label for="input2">Email Address</label>
+	                <input type="email" name="contact_email" class="form-control" id="input2">
+	              </div>
+	              <div class="form-group col-lg-4">
+	                <label for="input3">Phone Number</label>
+	                <input type="phone" name="contact_phone" class="form-control" id="input3">
+	              </div>
+	              <div class="clearfix"></div>
+	              <div class="form-group col-lg-12">
+	                <label for="input4">Message</label>
+	                <textarea name="contact_message" class="form-control" rows="6" id="input4"></textarea>
+	              </div>
+	              <div class="form-group col-lg-12">
+	                <input type="hidden" name="save" value="contact">
+	                <button type="submit" class="btn btn-primary">Submit</button>
+	              </div>
+              </div>
+            </form>
+        </div>
+
+        <div class="col-sm-4">
+          <h3>SinnoFlex Inc.</h3>
+          <h4>Simple Innovative Flexible</h4>
+          <p>
+            501 Byer Court<br>
+            San Ramon, CA 94582<br>
+          </p>
+          <p><i class="fa fa-phone"></i> <abbr title="Phone">P</abbr>: (925) 833-1148</p>
+          <p><i class="fa fa-envelope-o"></i> <abbr title="Email">E</abbr>: <a href="mailto:info@sinnoflex.com">info@sinnoflex.com</a></p>
+          <p><i class="fa fa-clock-o"></i> <abbr title="Hours">H</abbr>: Monday - Friday: 9:00 AM to 5:00 PM</p>
+          <ul class="list-unstyled list-inline list-social-icons">
+            <li class="tooltip-social facebook-link"><a href="#facebook-page" data-toggle="tooltip" data-placement="top" title="Facebook"><i class="fa fa-facebook-square fa-2x"></i></a></li>
+            <li class="tooltip-social linkedin-link"><a href="#linkedin-company-page" data-toggle="tooltip" data-placement="top" title="LinkedIn"><i class="fa fa-linkedin-square fa-2x"></i></a></li>
+            <li class="tooltip-social twitter-link"><a href="#twitter-profile" data-toggle="tooltip" data-placement="top" title="Twitter"><i class="fa fa-twitter-square fa-2x"></i></a></li>
+            <li class="tooltip-social google-plus-link"><a href="#google-plus-page" data-toggle="tooltip" data-placement="top" title="Google+"><i class="fa fa-google-plus-square fa-2x"></i></a></li>
+          </ul>
+        </div>
+
+      </div><!-- /.row -->
+
+    </div><!-- /.container -->
+
+    <div class="container-footer" >
+
+        <hr style="background-color:#c09853; height: 1px; ">
+
+      <footer style="background-color:  #c09853;">
+          <div class="row" style="padding: 10px;">
+          <div class="col-lg-12">
+            <p>Copyright &copy; SinnoFlex 2013</p>
+          </div>
+        </div>
+      </footer>
+
+    </div><!-- /.container -->
+
+    <!-- JavaScript -->
+    <script src="../js/jquery-1.10.2.js"></script>
+    <script src="../js/bootstrap.js"></script>
+    <script src="../js/modern-business.js"></script>
+
+  </body>
+</html>
